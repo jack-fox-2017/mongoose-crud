02ae6e70475686c52930d9930497c90f9786eb5a
rusli abdul gani <rusli.gani88@gmail.com>
2017-08-22 13:44:27 +0700

    init express gen, add model

---
+++ models/library.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var bookSchema = new Schema({
+  isbn: String,
+  title: String,
+  author: String,
+  category: String,
+  stock: Number
+},{
+  versionKey: false
+})
+
+var customerSchema = new Schema({
+  name: String,
+  memberid:String,
+  address: String,
+  zipcode: String,
+  phone: String 
+},{
+  versionKey: false
+})
+
+var transactionSchema = new Schema(memberid: {
+    type: schema.Types.ObjectId,
+    ref: 'Customer'
+  },
+  days: Number,
+  out_date: Date,
+  due_date: Date,
+  in_date: Date,
+  fine: Number,
+  booklist: [{
+    type: schema.Types.ObjectId,
+    ref: 'Book'
+  }] 
+}, {
+  versionKey: false
+})
+
+var bookModel = mongoose.model('Book', bookSchema);
+var customerModel = mongoose.model('Customer', customerSchema);
+var transactionModel = mongoose.model('Transaction', transactionSchema);
+
+
+module.exports = {
+  bookModel,
+  customerModel,
+  transactionModel
+}
