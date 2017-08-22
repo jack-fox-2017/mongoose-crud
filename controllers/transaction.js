598a6703d58110b45b403daacb64f99f3501f1e7
rusli abdul gani <rusli.gani88@gmail.com>
2017-08-22 18:58:50 +0700

    remove set from edit transactions

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -57,20 +57,11 @@ var deleteTransaction = ( req, res ) => {
 var updateTransaction = (req, res ) => {
   model.transactionModel.findByIdAndUpdate(req.params.id, 
     { 
-      $set: {
-        memberId: req.body.memberid,
-        days: req.body.days,
-        out_date: req.body.out_date,
-        due_date: req.body.due_date,
-        in_date: req.body.in_date,
-        fine: req.body.fine
-      },
       $push: {
         booklist: req.body.book_id
       }
   },( err, docs) => {
     if(!err) {
-      console.log(docs);
       res.send(docs)
     }else{
       res.send(err)
