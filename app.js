cdbce16f83b219706e1e3b9635bb75a81f0a01f8
gananggww <gananggww@gmail.com>
2017-08-30 13:40:50 +0700

    test deploy

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/library', (err)=>{
+mongoose.connect('mongo "mongodb://cluster0-shard-00-00-rcykk.mongodb.net:27017,cluster0-shard-00-01-rcykk.mongodb.net:27017,cluster0-shard-00-02-rcykk.mongodb.net:27017/test?replicaSet=Cluster0-shard-0" --authenticationDatabase admin --ssl --username gananggww --password qwerty1234', (err)=>{
   if(err){
     console.log(err);
   }else{
