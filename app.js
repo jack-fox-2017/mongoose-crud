21d97bd582ce16c6b8eaeda591aaea11f98055a6
Hacktiv8 <hacktiv8-5@Hacktiv8-5.local>
2017-08-30 12:46:26 +0700

    gcp

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/library')
+mongoose.connect('mongodb://rahmat:rahmat@cluster0-shard-00-00-gkut7.mongodb.net:27017,cluster0-shard-00-01-gkut7.mongodb.net:27017,cluster0-shard-00-02-gkut7.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
 
 var index = require('./routes/index');
 var books = require('./routes/books');
